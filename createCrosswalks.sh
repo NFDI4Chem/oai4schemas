bf5807ae74aa636d58cab49b563e0da616dc0598
Steffen Neumann <sneumann@ipb-halle.de>
2024-02-05 16:49:10 +0100

    Add datacite3 format and crosswalk for OpenAire

--- createCrosswalks.sh
+++ createCrosswalks.sh
@@ -6,14 +6,20 @@
 
 BACKEND="${1:-http://localhost:8081/oai-backend}"
 
-#The examples below use the linux command jq for encoding teh XSLT to JSON for adding it into the curl command
+#The examples below use the linux command jq for encoding the XSLT to JSON for adding it into the curl command
 #The package can be installed via yum install jq
 
-#Create Crosswalk from embedded JSON+LD to oai_dc
+# Create Crosswalk from embedded JSON+LD to oai_dc
 XSLT_JSON_ENCODED=`cat json_container2oai_dc.xslt | jq -Rsa .`
 curl -X POST -H 'Content-Type: application/json' \
   -i "$BACKEND/crosswalk" \
   --data '{"name":"json_container2OAI_DC_v09","formatFrom":"json_container","formatTo":"oai_dc","xsltStylesheet":'"$XSLT_JSON_ENCODED}"'}'
+
+# Create Crosswalk from embedded JSON+LD to oai_datacite
+XSLT_JSON_ENCODED=`cat json_container2oai_datacite3.xslt | jq -Rsa .`
+curl -X POST -H 'Content-Type: application/json' \
+  -i "$BACKEND/crosswalk" \
+  --data '{"name":"json_container2OAI_DATACITE_v3","formatFrom":"json_container","formatTo":"oai_datacite","xsltStylesheet":'"$XSLT_JSON_ENCODED}"'}'
 
 #Read all crosswalk
 curl -X GET "$BACKEND/crosswalk"
--- createFormats.sh
+++ createFormats.sh
@@ -10,9 +10,14 @@ BACKEND="${1:-http://localhost:8081/oai-backend}"
 ##
 ## Create Format
 ##
-
+printf "\n\nCreate Format json_container\n\n"
 curl -X POST -H 'Content-Type: application/json' -i "$BACKEND/format" \
   --data '{"metadataPrefix":"json_container","schemaLocation":"https://nfdi4chem.de/schemas/json-container/json-container.xsd","schemaNamespace":"http://nfdi4chem.de/schemas/json-container/1.0/","identifierXpath":""}'
+
+# Create (outdated) Datacite kernel-3 Format for OpenAire
+printf "\n\nCreate Format oai_datacite\n\n"
+curl -X POST -H 'Content-Type: application/json' -i "$BACKEND/format" \
+  --data '{"metadataPrefix":"oai_datacite","schemaLocation":"https://schema.datacite.org/meta/kernel-3/metadata.xsd","schemaNamespace":"http://datacite.org/schema/kernel-3","identifierXpath":""}'
 
 # Read all formats
 curl -X GET "$BACKEND/format"
